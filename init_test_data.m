3b9c6ab39c8d6e2f88915672d09b495b7fe4ee0a
unknown <bangiajo@BA2124WS24.WIN.ecf.utoronto.ca>
2014-11-30 15:29:23 -0500

    Added hidden test data + script initialize

---
+++ init_test_data.m
@@ -0,0 +1,8 @@
+load public_test_images.mat;
+load hidden_test_images.mat;
+
+if ~exist('hidden_test_images', 'var')
+  test_images = public_test_images;
+else
+  test_images = cat(3, public_test_images, hidden_test_images);
+end
